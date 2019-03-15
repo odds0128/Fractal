de3aa1c4b390486faa428b29471e39eea3b10913
Ryoya Funato <r.funato@Nuviana.local>
2019-03-15 19:49:56 +0900

    :add :Prepare item table and model with mysql
    個別のTodoアイテムを保存するデータベースのテーブルとモデルを定義．

---
+++ db/migrate/20190315093530_create_items.rb
@@ -0,0 +1,11 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string  :name, null: false
+      t.boolean :is_done, default: false, null: false
+      t.string  :ancestry, index: true
+
+      t.timestamps
+    end
+  end
+end
